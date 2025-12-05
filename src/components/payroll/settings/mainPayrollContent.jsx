b418799862eb4bdf0d45926d4ab1123efd969ef0
gachonejohn <ceogachone@gmail.com>
2025-12-05 22:43:01 +0300

    stashedd

--- src/components/payroll/settings/mainPayrollContent.jsx
+++ src/components/payroll/settings/mainPayrollContent.jsx
@@ -3,12 +3,12 @@ import PAYETax from "./PAYETax";
 import NSSF from "./NSSF";
 import SHIF from "./SHIF";
 import AHL from "./AHL";
-import OvertimePayroll from "../Overtime Payroll";
-import PayrollRules from "../Payroll Rules";
+import OvertimePayroll from "../OvertimePayroll";
+import PayrollRules from "../PayrollRules";
 import Allowances from "./Allowances";
 import Deductions from "./Deductions";
-import StatutoryDeductions from "./Statutory Deductions";
-import AdjustmentRates from "../Adjustment Rates";
+import StatutoryDeductions from "./StatutoryDeductions";
+import AdjustmentRates from "../AdjustmentRates";
 
 export default function MainPayrollContent() {
   const [activeTab, setActiveTab] = useState("paye");
